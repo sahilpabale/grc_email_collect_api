7f9cc03792f3dbcb8ac95ac35fd6d4abcca4db41
sahilpabale <dev.sahilpabale@gmail.com>
2021-04-17 09:45:52 +0530

    added cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { Client } = require("pg");
+const cors = require("cors");
 
 if (process.env.NODE_ENV != "production") {
   require("dotenv").config();
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
